93c7ecd9cfc8b5c425ec8a14ea71d0f11251ced6
AMA <alexanderabram6440@gmail.com>
2020-07-29 11:20:53 -0500

    Updates

---
+++ precache-manifest.ee1674b673be5820e2a3483540ad7a18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cba16b826d216634b523405a5103db",
+    "url": "/pet-project/index.html"
+  },
+  {
+    "revision": "7e935bb7a65c8ca57d6f",
+    "url": "/pet-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b08be1c8595edf71041c",
+    "url": "/pet-project/static/js/2.345ae265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pet-project/static/js/2.345ae265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e935bb7a65c8ca57d6f",
+    "url": "/pet-project/static/js/main.7a44c7f2.chunk.js"
+  },
+  {
+    "revision": "7c3694e52830c6d8726d",
+    "url": "/pet-project/static/js/runtime-main.7099945d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pet-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
