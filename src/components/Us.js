7e260e9b6e48e535906ce23275ae4a5c50689037
AMA <alexanderabram6440@gmail.com>
2020-08-03 11:30:56 -0500

    updated my about me

--- src/components/Us.js
+++ src/components/Us.js
@@ -6,6 +6,7 @@ import placeholder1 from "../images/placeholder1.jpg";
 import Ray from "../images/Ray.jpg";
 import Grigore from "../images/Grigore.jpeg";
 import Violet from "../images/Violet.png";
+import Alex from "../images/Alex.jpg";
 
 import "./css/style.css";
 import "./pages/UsStyle.css";
@@ -23,20 +24,19 @@ export default class Us extends Component {
                 <div className="col s12">
                     <div className="col s12" id="thisIsUs">
                         <h3><b>This is Us</b></h3>
-                </div>
+                    </div>
                     <div className="container" id="ourSection">
                         <div className="col s12" id="filler">
 
                             <div className="row">
                                 <div className="col s6">
                                     <div id="Alex">
-                                    <p  className="right-align">This is Alex's info</p>
+                                        <p className="right-align">Alex has always loved dogs. He currently has three four-legged members of the family, Teddy, Nelli, and Ralphie.</p>
                                     </div>
                                 </div>
                                 <div className="col">
                                     <div id="Alex">
-                                        <img id="pic" className="left" src={placeholder1} alt="Alex" />
-                                        This is Alex's Picture
+                                        <img id="pic" className="left" src={Alex} alt="Alex" />
                                     </div>
                                 </div>
                             </div>
@@ -48,7 +48,7 @@ export default class Us extends Component {
                                         Web Developer
                                         <img id="headShot" src={Grigore} alt="Grigore" />
 
-                                      
+
                                     </div>
                                 </div>
                                 <div className="col s6">
@@ -56,16 +56,16 @@ export default class Us extends Component {
 
                                         Grigore comes from a country where people have a cruel relation with pets leaving them outside all year round and using them only as security.I hope this app will be useful and will help stop violence against animals and will make happy those who love pets.
 
-                                    <p  className="left-align">This is Grigore's Info</p>
+                                    <p className="left-align">This is Grigore's Info</p>
 
                                     </div>
                                 </div>
                             </div>
-                    
+
                             <div className="row">
                                 <div className="col s6">
                                     <div id="Ray">
-                                        <p  className="right-align">Ray has spent the last 11 years as a technical
+                                        <p className="right-align">Ray has spent the last 11 years as a technical
                                         trainer (less 1 in the middle attempting  to do sales). His new career journey to becoming a full-stack web developer has
                                         been very exciting.</p>
                                     </div>
@@ -86,7 +86,7 @@ export default class Us extends Component {
                                 </div>
                                 <div className="col s6">
                                     <div id="Vi">
-                                    <p  className="left-align">UX/UI designer hopeful, recent Vanderbilt University Coding Boot Camp graduate. cat mom, dogs are okay i guess.</p>
+                                        <p className="left-align">UX/UI designer hopeful, recent Vanderbilt University Coding Boot Camp graduate. cat mom, dogs are okay i guess.</p>
 
                                     </div>
                                 </div>
