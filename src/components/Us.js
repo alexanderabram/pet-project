5372e6ca7d41105fa42dc25f57bb6f78339a710a
Ray Phillips <raydphillips3@gmail.com>
2020-08-03 10:42:03 -0500

    fixed homepage, cleaned up some code on the member.js page

--- src/components/Us.js
+++ src/components/Us.js
@@ -28,41 +28,44 @@ export default class Us extends Component {
                             <div className="row">
                                 <div className="col s6">
                                     <div id="Alex">
-                                        This is Alex's info
+                                    <p  className="right-align">This is Alex's info</p>
                                     </div>
                                 </div>
                                 <div className="col s6">
                                     <div id="Alex">
-                                        <img src={placeholder1} alt="Alex" />
+                                        <img id="pic" className="left" src={placeholder1} alt="Alex" />
                                         This is Alex's Picture
                                     </div>
                                 </div>
                             </div>
 
+
                             <div className="row">
                                 <div className="col s6">
                                     <div id="Grigore">
                                         This is Grigore's Picture
-                                        <img src={placeholder1} alt="Grigore" />
+                                        <img id="pic" className="right" src={placeholder1} alt="Grigore" />
                                     </div>
                                 </div>
                                 <div className="col s6">
                                     <div id="Grigore">
-                                        This is Grigore's Info
+                                    <p  className="left-align">This is Grigore's Info</p>
                                     </div>
                                 </div>
                             </div>
+                            
 
                             <div className="row">
                                 <div className="col s6">
                                     <div id="Ray">
-                                        This is Ray's info
+                                        <p  className="right-align">Ray has spent the last 11 years as a technical
+                                        trainer (less 1 in the middle attempting  to do sales). His new career journey to becoming a full-stack web developer has
+                                        been very exciting.</p>
                                     </div>
                                 </div>
                                 <div className="col s6">
                                     <div id="Ray">
-                                        This is Ray's Picture
-                                        <img src={Ray} alt="Ray" />
+                                        <img id="pic" className="left" src={Ray} alt="Ray" />
                                     </div>
                                 </div>
                             </div>
@@ -71,12 +74,12 @@ export default class Us extends Component {
                                 <div className="col s6">
                                     <div id="Vi">
                                         This is Vi's Picture
-                                        <img src={placeholder1} alt="Vi" />
+                                        <img id="pic" className="right" src={placeholder1} alt="Vi" />
                                     </div>
                                 </div>
                                 <div className="col s6">
                                     <div id="Vi">
-                                        This is Vi's Info
+                                    <p  className="left-align">This is Vi's Info</p>
                                     </div>
                                 </div>
                             </div>
