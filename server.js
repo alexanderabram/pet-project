36caa41548a78002d7166d3b7f0c8ac127b20cd3
AMA <alexanderabram6440@gmail.com>
2020-08-04 16:46:29 -0500

    added passport to server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const passport = require('passport');
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
