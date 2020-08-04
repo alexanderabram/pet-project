ec9c7c327ca85783b642914e12c33032d41b13a6
AMA <alexanderabram6440@gmail.com>
2020-08-04 13:27:37 -0500

    added name and pw to schema

--- models/user.js
+++ models/user.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userScehma = new Schema ({
-    
+    name: { type: String, required: true },
+    password: { type: String, required: true}
 })
