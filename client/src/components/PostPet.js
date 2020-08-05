07cb4defdc212a2d906cb935d5d315179ed02630
Ray Phillips <raydphillips3@gmail.com>
2020-08-05 10:21:55 -0500

    fixed class tags

--- client/src/components/PostPet.js
+++ client/src/components/PostPet.js
@@ -20,56 +20,56 @@ export default class Listpet extends Component {
 
     render() {
         return (
-            <div class="container" id="list">
-                <div class="row">
-                    <form class="col s12" >
+            <div className="container" id="list">
+                <div className="row">
+                    <form className="col s12" >
                         <div>
                             <h3>Post Your Pet</h3><h5 id="free">We'll Find A Great Match!</h5>
                         </div>
 
-                        <div class="row">
-                            <form class="col s11">
-                                <div class="row">
-                                    <div class="input-field col s6"  id="petName">
-                                        <input id="icon_prefix" type="text" class="validate" />
+                        <div className="row">
+                            <form className="col s11">
+                                <div className="row">
+                                    <div className="input-field col s6"  id="petName">
+                                        <input id="icon_prefix" type="text" className="validate" />
                                         <label for="icon_prefix" ><GiPawPrint size="30px" color="black" />    Your Pet's Name</label>
                                     </div>
-                                    <div class="input-field col s6" id="petType">
-                                        <input id="icon_prefix" type="text" class="validate" />
+                                    <div className="input-field col s6" id="petType">
+                                        <input id="icon_prefix" type="text" className="validate" />
                                         <label for="icon_prefix" ><GiVirtualMarker size="30px" color="black" />    Your Pet's Type (ex. dog)</label>
                                     </div>
                                 </div>
                             </form>
                         </div>
 
-                        <div class="row">
-                            <form class="col s12">
-                                <div class="row">
-                                    <div class="input-field col s11"  id="petBio">
-                                        <textarea id="textarea1" class="materialize-textarea"></textarea>
+                        <div className="row">
+                            <form className="col s12">
+                                <div className="row">
+                                    <div className="input-field col s11"  id="petBio">
+                                        <textarea id="textarea1" className="materialize-textarea"></textarea>
                                         <label for="textarea1" ><GiTalk size="30px" color="black" />        Tell Us About Your Pet</label>
                                     </div>
                                 </div>
                             </form>
                         </div>
 
-                        <div class="row">
-                            <form class="col s11">
-                                <div class="row">
-                                    <div class="input-field col s6" id="petOwner">
-                                        <input id="icon_prefix" type="text" class="validate" />
+                        <div className="row">
+                            <form className="col s11">
+                                <div className="row">
+                                    <div className="input-field col s6" id="petOwner">
+                                        <input id="icon_prefix" type="text" className="validate" />
                                         <label for="icon_prefix" ><GiPerson size="30px" color="black" />    Your Name</label>
                                     </div>
-                                    <div class="input-field col s6" id="petContact">
-                                        <input id="icon_prefix" type="text" class="validate" />
+                                    <div className="input-field col s6" id="petContact">
+                                        <input id="icon_prefix" type="text" className="validate" />
                                         <label for="icon_prefix" ><GiSmartphone size="30px" color="black" />    Best Way To Contact You</label>
                                     </div>
                                 </div>
                             </form>
                         </div>
-                        <div class="row" id="listBtn">
-                            <div class="input-field col s6" className="center-align">
-                                <a class="waves-effect waves-light btn-large">Post Pet    <GiLoveHowl size="30px" color="black" /></a>
+                        <div className="row" id="listBtn">
+                            <div className="input-field col s6" className="center-align">
+                                <a className="waves-effect waves-light btn-large">Post Pet    <GiLoveHowl size="30px" color="black" /></a>
                             </div>
                         </div>
 
